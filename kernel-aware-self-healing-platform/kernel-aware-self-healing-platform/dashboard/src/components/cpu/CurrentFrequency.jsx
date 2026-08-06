735c9f90d69f48ab0d6fe03b0f66a77ef1e14188
videesha <videeshanavodi1234@gmail.com>
2026-08-06 09:43:52 +0530

    new changes

--- kernel-aware-self-healing-platform/kernel-aware-self-healing-platform/dashboard/src/components/cpu/CurrentFrequency.jsx
+++ kernel-aware-self-healing-platform/kernel-aware-self-healing-platform/dashboard/src/components/cpu/CurrentFrequency.jsx
@@ -2,7 +2,7 @@ function CurrentFrequency() {
     return (
         <iframe
             title="Current Frequency"
-            src="http://localhost:3000/d-solo/cpu-performance-dash/cpu-and-performance?var-interval=$__auto&orgId=1&from=1785318765618&to=1785320565618&timezone=browser&var-filter0=&var-DS_PROMETHEUS=ffsoo1vfqiku8a&var-instance=$__all&refresh=5s&panelId=panel-205"
+            src="http://localhost:3000/d-solo/cpu-performance-dash/cpu-and-performance?var-interval=$__auto&orgId=1&timezone=browser&var-filter0=&var-DS_PROMETHEUS=ffsoo1vfqiku8a&var-instance=$__all&refresh=5s&panelId=panel-205&from=now-30m&to=now"
             width="100%"
             height="314"
             frameBorder="0"
--- kernel-aware-self-healing-platform/kernel-aware-self-healing-platform/dashboard/src/components/cpu/PerformanceOverview.jsx
+++ kernel-aware-self-healing-platform/kernel-aware-self-healing-platform/dashboard/src/components/cpu/PerformanceOverview.jsx
@@ -2,7 +2,7 @@ function PerformanceOverview() {
     return (
         <iframe
             title="Performance Overview"
-            src="http://localhost:3000/d-solo/cpu-performance-dash/cpu-and-performance?var-interval=$__auto&orgId=1&from=1785318380606&to=1785320180606&timezone=browser&var-filter0=&var-DS_PROMETHEUS=ffsoo1vfqiku8a&var-instance=$__all&refresh=5s&panelId=panel-201"
+            src="http://localhost:3000/d-solo/cpu-performance-dash/cpu-and-performance?var-interval=$__auto&orgId=1&timezone=browser&var-filter0=&var-DS_PROMETHEUS=ffsoo1vfqiku8a&var-instance=$__all&refresh=5s&panelId=panel-201&from=now-30m&to=now"
             width="100%"
             height="315"
             frameBorder="0"
