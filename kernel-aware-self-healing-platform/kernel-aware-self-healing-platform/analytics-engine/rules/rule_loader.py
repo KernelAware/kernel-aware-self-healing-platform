0c40932219f120165a8bffa7828fc689daaa4093
theekshana-khp <pasindutheekshana903@gmail.com>
2026-08-15 13:51:18 +0530

    make all relate to disply process and service on grfana and react dashboard

--- kernel-aware-self-healing-platform/kernel-aware-self-healing-platform/analytics-engine/rules/rule_loader.py
+++ kernel-aware-self-healing-platform/kernel-aware-self-healing-platform/analytics-engine/rules/rule_loader.py
@@ -1,2 +1,41 @@
-def load_rules():
+from database.connection import get_db_connection
+
+
+def load_cpu_rules():
+    return load_rules_by_metric("cpu")
+
+def load_memory_rules():
+    return load_rules_by_metric("memory")
+
+def load_disk_rules():
+    return load_rules_by_metric("disk")
+
+def load_network_rules():
+    return load_rules_by_metric("network")
+
+def load_process_rules():
+    return load_rules_by_metric("process")
+
+def load_rules_by_metric(metric_type):
+    connection = get_db_connection()
+
+    try:
+        cursor = connection.cursor()
+
+        query = """
+            SELECT *
+            FROM rules
+            WHERE metric_type = %s
+            AND enabled = TRUE
+        """
+
+        cursor.execute(query, (metric_type,))
+        return cursor.fetchall()
+
+    finally:
+        cursor.close()
+        connection.close()
+
+def load_rules(metrics):
     pass
+
