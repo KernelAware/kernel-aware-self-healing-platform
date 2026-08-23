c2fbf3d6e8c20498ce679802b386af0ad47171f0
theekshana-khp <pasindutheekshana903@gmail.com>
2026-08-23 17:50:21 +0530

    make backend user rules store

--- kernel-aware-self-healing-platform/kernel-aware-self-healing-platform/backend/app/services/user_rule_service.py
+++ kernel-aware-self-healing-platform/kernel-aware-self-healing-platform/backend/app/services/user_rule_service.py
@@ -1,2 +1,25 @@
+from models.user_rules.rule import Rule
+
+
 def user_rules_service(userRules):
-    pass
+    rule = Rule(
+        name=userRules["ruleName"],
+        status="ENABLED" if userRules["enabled"] else "DISABLED",
+        priority=userRules["priority"],
+        severity=userRules["severity"],
+        owner=userRules["owner"],
+        environment=userRules["environment"],
+        region=userRules["region"],
+        monitor_type=userRules["monitorSource"],
+        target_type=userRules["targets"][0]["type"],
+        target=userRules["targets"][0]["name"]
+    )
+
+    print(rule.name)
+    print(rule.status)
+    print(rule.priority)
+    print(rule.monitor_type)
+    print(rule.target_type)
+    print(rule.target)
+
+    return rule
