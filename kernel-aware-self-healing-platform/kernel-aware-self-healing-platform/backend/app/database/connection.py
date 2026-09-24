dd90d1090fb36f112fa2805498a5a14156540673
dinethnk <dinethnk@gmail.com>
2026-09-24 22:13:02 +0530

    create rule object passed to the database

--- kernel-aware-self-healing-platform/kernel-aware-self-healing-platform/backend/app/database/connection.py
+++ kernel-aware-self-healing-platform/kernel-aware-self-healing-platform/backend/app/database/connection.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import DeclarativeBase, sessionmaker
 
-DATABASE_URL = "mysql+mysqlconnector://pasindu:1234@localhost:3306/user_rules"
+DATABASE_URL = "mysql+mysqlconnector://dineth:1234@localhost:3306/user_rules"
 
 engine = create_engine(DATABASE_URL)
 
